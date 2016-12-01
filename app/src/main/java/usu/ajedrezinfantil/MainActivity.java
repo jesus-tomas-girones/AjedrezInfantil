56c5d9acca942a2e4dedb44a748ea7c6c8a33c01
raulvibo <raulvibo@gmail.com>
2016-12-01 13:31:40 +0100

    Comentario de prueba

--- app/src/main/java/usu/ajedrezinfantil/MainActivity.java
+++ app/src/main/java/usu/ajedrezinfantil/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity  {
+    // ESTO ES UNA PRUEBA
 
     //Todo: (Raúl) Solicitar permiso grabación audio
 
