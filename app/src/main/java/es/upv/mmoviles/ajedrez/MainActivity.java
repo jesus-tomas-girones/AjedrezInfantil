a8cfde922988643f9261a03ecc7b4e2da821ae6b
raulvibo <raulvibo@gmail.com>
2016-12-03 03:59:46 +0100

    Solicitar permiso grabación audio en MainActivity Avatar aparece en la pantalla inicial

--- app/src/main/java/es/upv/mmoviles/ajedrez/MainActivity.java
+++ app/src/main/java/es/upv/mmoviles/ajedrez/MainActivity.java
@@ -1,12 +1,21 @@
 package es.upv.mmoviles.ajedrez;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.media.AudioManager;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity  {
+    private VistaAvatar avatar;
+    private final int REQUEST_RECORD_AUDIO = 0;
+
     // ESTO ES UNA PRUEBA
 
     //Todo: (Raúl) Solicitar permiso grabación audio
@@ -23,6 +32,18 @@ public class MainActivity extends AppCompatActivity  {
         super.onCreate(savedInstanceState);
         setVolumeControlStream(AudioManager.STREAM_MUSIC);
         setContentView(R.layout.activity_main);
+        avatar = (VistaAvatar) findViewById(R.id.vistaAvatar);
+        avatar.setActividad(this);
+        presentacion();
+    }
+
+    public void presentacion() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED) {
+            avatar.habla(R.raw.presentacion);
+        } else {
+            solicitarPermisoRecordAudio();
+        }
     }
 
     public void boton1(View v) {
@@ -39,6 +60,43 @@ public class MainActivity extends AppCompatActivity  {
 
     public void boton4(View v) {
         startActivity(new Intent(this, MoverDamaActivity.class));
+    }
+
+    void solicitarPermisoRecordAudio() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                Manifest.permission.RECORD_AUDIO)) {
+            Snackbar snackbar = Snackbar.make(avatar, "Sin el permiso grabación de audio,\n"
+                    + "no puedo mostrarte el avatar hablando.", Snackbar.LENGTH_INDEFINITE);
+            View snackbarView = snackbar.getView();
+            TextView textView = (TextView) snackbarView.findViewById(android.support.design.R.id.snackbar_text);
+            textView.setMaxLines(2);
+            snackbar.setAction("OK", new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{
+                            Manifest.permission.RECORD_AUDIO}, REQUEST_RECORD_AUDIO);
+                }
+            }).show();
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO}, REQUEST_RECORD_AUDIO);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == REQUEST_RECORD_AUDIO) {
+            if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                presentacion();
+            } else {
+                Snackbar snackbar = Snackbar.make(avatar, "Sin el permiso grabación de audio,\n"
+                        + "no puedo mostrarte el avatar hablando.", Snackbar.LENGTH_LONG);
+                View snackbarView = snackbar.getView();
+                TextView textView = (TextView) snackbarView.findViewById(android.support.design.R.id.snackbar_text);
+                textView.setMaxLines(2);
+                snackbar.show();
+                finish();
+            }
+        }
     }
 
 }
